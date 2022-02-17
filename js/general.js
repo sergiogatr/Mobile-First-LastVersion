e09c43cd08df5021a96f44e38f19dafccad1a8ee
Sergio García <71490024+sergiogatr@users.noreply.github.com>
2022-02-17 10:44:45 +0100

    Tada

---
+++ js/general.js
@@ -0,0 +1,7 @@
+// function calcular() {
+//     var ventana_ancho = $(window).width();
+//     var ventana_alto = $(window).height();
+//     $("h4").text("Viewport dimensions " + ventana_ancho + "x" + ventana_alto);   
+// }
+
+// setInterval(calcular, 0);
